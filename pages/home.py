9c3350af79f883c3b75cad844ef9900a6b57f308
Augusto Aleman <a01660702@tec.mx>
2024-03-07 18:50:38 -0600

    Updated footer and navbar's style

--- pages/home.py
+++ pages/home.py
@@ -8,6 +8,51 @@ from dash import callback
 dash.register_page(__name__, path='/')
 
 layout = html.Div([
-        html.H1('Holi esta es la pagina principal :)', className='title'),
-])
+    html.Div([
+
+        # Barra de estado
+         html.Div([
+             html.Div([
+                 html.Div([
+                     html.A('32'),
+                     html.A('Dias'),
+                 ], className= 'status-bar-right-text'),
+                  html.Hr(className='status-bar-right-vertical-line'),
+             ], className = 'status-bar-right'),
+             
+             html.Div([
+                 html.Div([
+                    html.A('Estado del Sistema: Activo'),
+                    html.A(['Último Riego: 14:45']),
+                 ], className= 'status-bar-left-text'),
+                 html.Div(html.Img(src = 'assets/src/load-button.png', className='load-button'), className='load-button-container')
+             ], className = 'status-bar-left')
+        ], className = 'status-bar'),
+
+        # Cuadro Torre 
+        html.Div([
+
+            html.Div(html.Img(src = 'assets/src/aeroponic-tower.png', className='aero-image'), className='aero-image-container'),
+
+            html.Div([
+                html.Div([ html.A('Eleva tu experiencia de cultivo con Aeroponia'),], className = 'tower-description-top'),
+                html.Div([
+                    html.H3('Torre'),
+                    html.A('Planta Baja')
+                ], className = 'tower-description-bottom'),
+            ], className = 'tower-description'),
+
+            html.Div([
+                html.A('hola')
+            ], className='tower-buttons', 
+        style={'backgroundImage': 'url(\'assets/src/leafs.png\')', 
+               'backgroundSize': 'cover'}),
+
+        ], className='tower-container', 
+        style={'backgroundImage': 'url(\'assets/src/gray-background.png\')', 
+               'backgroundSize': 'cover'})
+
+    ], className = 'main'),
+    html.H1('Holi esta es la pagina principal :)', className='title'),
+], className='parent-container')
 
